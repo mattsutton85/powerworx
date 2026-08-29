191644b872b0bb9d8e94bc34b02fda35e1e5ce0a
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-08-29 16:36:26 +0100

    Update pwx.globals.d.ts

--- Simhub/Overhead/pwx.globals.d.ts
+++ Simhub/Overhead/pwx.globals.d.ts
@@ -1,3 +1,14 @@
 declare function $prop(
     name: string
 ): unknown
+
+declare function _apxNumber( 
+    value: any, 
+    fallback: number|string,
+    decimals: number,
+): number|string
+
+declare function _apxString(
+    value: any,
+    fallback: string
+): string
