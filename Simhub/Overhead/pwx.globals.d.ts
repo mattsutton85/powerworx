3a2b2572ad2633184928da0bd49fca1102717bda
Matt Sutton <matt@teamgleam.co.uk>
2026-08-30 12:45:23 +0100

    update to new lib

--- Simhub/Overhead/pwx.globals.d.ts
+++ Simhub/Overhead/pwx.globals.d.ts
@@ -2,13 +2,22 @@ declare function $prop(
     name: string
 ): unknown
 
-declare function _pwxNumber( 
-    value: any, 
+declare function _pwxTime(
+    seconds: number,
+    format: | 'LAP'
+        | 'SESSION'
+        | 'DELTA'
+): string
+
+declare function _pwxNumber(
+    value: any,
     fallback: number|string,
     decimals: number,
 ): number|string
 
 declare function _pwxString(
     value: any,
-    fallback: string
+    fallback?: string
 ): string
+
+declare function _pwxUiColour( theme: object, element: string, state: string ): string
--- Simhub/Overhead/pwx.utils.js
+++ Simhub/Overhead/pwx.utils.js
@@ -1,9 +1,48 @@
-function _pwxString(value){
-    return String(value)
+function _pwxNumber( val, fallback = 0, decimals = 0 ){
+    if (val == null || val === "" || isNaN(val)) return fallback
+    return Number(val).toFixed(decimals);
 }
 
-function _pwxNumber( val, fallback = 0, decimals = 0 ){
-    if (val == null || val === "" || isNaN(val))
-        return fallback;
-    return Number(val).toFixed(decimals);
+function _pwxString( val, fallback = '' ){
+    if( !val ) return fallback
+    return String(val)
+}
+
+function _pwxTime( seconds, format )
+{
+    if (seconds == null || isNaN(seconds) || seconds === 0 ) {
+        switch( format ){
+            case 'LAP':
+                return '--:--.---'
+            case 'SESSION':
+                return '--:--'
+            case 'DELTA':
+                if( seconds === 0 ){
+                    return '0.000'
+                }
+                return '-.---'
+        }
+    }
+
+    seconds = Math.abs( Number(seconds) );
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const wholeSeconds = Math.floor(remainingSeconds);
+    const milliseconds = Math.round((remainingSeconds - wholeSeconds) * 1000);
+
+    switch( format ){
+        case 'LAP':
+            return String(minutes).padStart(2,"0") + ":" + String(wholeSeconds).padStart(2, "0") + "." + String(milliseconds).padStart(3, "0");
+        case 'SESSION':
+            return String(minutes).padStart(2,"0") + ":" + String(wholeSeconds).padStart(2, "0");
+        case 'DELTA':
+            const str = ( seconds < 0 ) ? '-' : '+'
+            return str + String(wholeSeconds) + "." + String(milliseconds).padStart(3, "0");
+    }
+}
+
+function _pwxUiColour( theme, element, state )
+{
+    return theme[element][state]
 }
