69fcf298cd2e3b3008013d167bd99ba8d7138075
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-02 16:38:22 +0100

    Update pwx.data.session.js

--- Simhub/PWX-Core/pwx.data.session.js
+++ Simhub/PWX-Core/pwx.data.session.js
@@ -5,11 +5,20 @@ pwx.data.session.incident = pwx.data.incident || {}
 pwx.data.session.lap = pwx.data.lap || {}
 pwx.data.session.time = pwx.data.time || {}
 
+const _pwxCoreSessionStates = {
+    practice: ['Practice'],
+    qualifying: ['Lone Qualify'],
+    race: ['Race']
+}
+
 pwx.data.session.number = function(){
     return _pwxProp( pwx.config.session.number )
 }
 pwx.data.session.name = function(){
     return _pwxProp( pwx.config.session.name )
+}
+pwx.data.session.is = function(state){
+    return _pwxBoolean( (_pwxCoreSessionStates[state].includes(_pwxProp(pwx.config.session.name))))
 }
 pwx.data.session.incident.limit = function(){
     return _pwxProp( pwx.config.session.incident.limit )
