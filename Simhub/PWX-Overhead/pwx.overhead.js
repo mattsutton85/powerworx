d335b551eefb1d64d386d5e7b3eb33c204589a84
Matt Sutton <matt@teamgleam.co.uk>
2026-09-03 10:55:54 +0100

    add overhead base and context bar

--- Simhub/PWX-Overhead/pwx.overhead.js
+++ Simhub/PWX-Overhead/pwx.overhead.js
@@ -1,3 +1,18 @@
 var pwx = pwx || {}
 pwx.core = pwx.core || {}
 pwx.overhead = pwx.overhead || {}
+pwx.overhead.incident = pwx.overhead.incident || {}
+
+pwx.overhead.incident.value = function(){
+    return _pwxNumber( pwx.core.data.driver.incidents(), 0 )
+}
+pwx.overhead.incident.colourBackground = function(){
+    return 'Red'
+}
+pwx.overhead.incident.colourValue = function(){
+    return 'White'
+}
+pwx.overhead.incident.colourLabel = function(){
+    return 'White'
+}
+
