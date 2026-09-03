df59969399b9f9a516fb3c0ed90b58c7b5c7044c
Matt Sutton <matt@teamgleam.co.uk>
2026-09-03 13:15:00 +0100

    add shift

--- Simhub/PWX-Overhead/pwx.overhead.js
+++ Simhub/PWX-Overhead/pwx.overhead.js
@@ -1,10 +1,14 @@
 var pwx = pwx || {}
 pwx.core = pwx.core || {}
 pwx.overhead = pwx.overhead || {}
+
 pwx.overhead.incident = pwx.overhead.incident || {}
+pwx.overhead.lap = pwx.overhead.lap || {}
+pwx.overhead.remaining = pwx.overhead.remaining || {}
+pwx.overhead.position = pwx.overhead.position || {}
 
 pwx.overhead.incident.value = function(){
-    return _pwxNumber( pwx.core.data.driver.incidents(), 0 )
+    return _pwxString( pwx.core.data.driver.incidents(), '0' )
 }
 pwx.overhead.incident.colourBackground = function(){
     return 'Red'
@@ -16,3 +20,6 @@ pwx.overhead.incident.colourLabel = function(){
     return 'White'
 }
 
+pwx.overhead.incident.lap = function(){
+    return _pwxString( pwx.core.data.lap.number(), '-' )
+}
