dc5eb0e939d4d624f6d62457e765631250a86c02
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-03 16:37:48 +0100

    Update pwx.core.data.flag.js

--- Simhub/PWX-Core/pwx.core.data.flag.js
+++ Simhub/PWX-Core/pwx.core.data.flag.js
@@ -2,3 +2,11 @@ var pwx = pwx || {}
 pwx.core = pwx.core || {}
 pwx.core.data = pwx.core.data || {}
 pwx.core.data.flag = pwx.core.data.flag || {}
+
+pwx.core.data.flag.current = function(){
+
+}
+
+pwx.core.data.flag.is = function( state ){
+
+}
