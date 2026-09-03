84c4d1320825e2d0e6174e0aec46b49cca210169
Matt Sutton <matt@teamgleam.co.uk>
2026-09-03 15:45:54 +0100

    rework vars

--- Simhub/PWX-Core/pwx.core.data.opponent.js
+++ Simhub/PWX-Core/pwx.core.data.opponent.js
@@ -3,27 +3,53 @@ pwx.core = pwx.core || {}
 pwx.core.data = pwx.core.data || {}
 pwx.core.data.opponent = pwx.core.data.opponent || {}
 
+// Retrieve opponent ID
 pwx.core.data.opponent.id = function( relative, pos ){
-    return _pwxProp( pwx.core.config.opponent.id, relative, pos );
-}
-pwx.core.data.opponent.name = function( relative, pos ){
-    return _pwxProp( pwx.core.config.opponent.name, relative, pos );
-}
-pwx.core.data.opponent.iRating = function( relative, pos ){
-    return _pwxProp( pwx.core.config.opponent.iRating, relative, pos );
-}
-pwx.core.data.opponent.position = function( relative, pos ){
-    return _pwxProp( pwx.core.config.opponent.position, relative, pos );
-}
-pwx.core.data.opponent.gap = function( relative, pos ){
-    return _pwxProp( pwx.core.config.opponent.gap, relative, pos );
+    return _pwxProp( pwx.core.config.opponent.id,
+        __pwxCorePrepareOpponentDirection( relative ),
+        __pwxCorePrepareOpponentPosition( pos )
+    );
 }
 
+// Retrieve opponent name
+pwx.core.data.opponent.name = function( relative, pos ){
+    return _pwxProp( pwx.core.config.opponent.name,
+        __pwxCorePrepareOpponentDirection( relative ),
+        __pwxCorePrepareOpponentPosition( pos )
+    )
+}
+
+// Retrieve opponent iRating
+pwx.core.data.opponent.iRating = function( relative, pos ){
+    return _pwxProp( pwx.core.config.opponent.iRating,
+        __pwxCorePrepareOpponentDirection( relative ),
+        __pwxCorePrepareOpponentPosition( pos )
+    )
+}
+
+// Retrieve opponent position
+pwx.core.data.opponent.position = function( relative, pos ){
+    return _pwxProp( pwx.core.config.opponent.position,
+        __pwxCorePrepareOpponentDirection( relative ),
+        __pwxCorePrepareOpponentPosition( pos )
+    )
+}
+
+// Retrieve opponent gap
+pwx.core.data.opponent.gap = function( relative, pos ){
+    return _pwxProp( pwx.core.config.opponent.gap,
+        __pwxCorePrepareOpponentDirection( relative ),
+        __pwxCorePrepareOpponentPosition( pos )
+    )
+}
+
+/*
+ * Utility functions
+ */
 function __pwxCorePrepareOpponentDirection(direction){
     return String(direction).charAt(0).toUpperCase()
         + String(direction).slice(1).toLowerCase();
 }
-
 function __pwxCorePrepareOpponentPosition( pos ){
     return String(parseInt(pos, 10)).padStart(2, '0');
 }
