fdfa98a96339d08faf80245edc373a0383b21f53
Matt Sutton <matt@teamgleam.co.uk>
2026-09-04 11:13:38 +0100

    add shift led

--- Simhub/PWX-Dash/pwx.dash.js
+++ Simhub/PWX-Dash/pwx.dash.js
@@ -4,18 +4,17 @@ pwx.dash.shift = pwx.dash.shift || {}
 pwx.dash.shift.led = pwx.dash.shift.led || {}
 
 pwx.dash.shift.led.state = function( ledNum ){
-
     const value = pwx.core.data.car.gearbox.shift.progress()
-
     let on = false
-    if( value <= ledNum ){
+    let colour = 'Transparent'
+    if( value >= ledNum ){
         on = true
+        colour = 'Yellow'
     }
-
     return {
         num: ledNum,
         on: on,
-        colour: 'Transparent',
+        colour: colour,
         blink: false
     }
 }
