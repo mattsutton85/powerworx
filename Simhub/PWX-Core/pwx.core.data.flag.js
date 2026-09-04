fd890b676e6771ac1fdb50a6e3d5878751378f97
Matt Sutton <matt@teamgleam.co.uk>
2026-09-04 06:42:11 +0100

    add flag states

--- Simhub/PWX-Core/pwx.core.data.flag.js
+++ Simhub/PWX-Core/pwx.core.data.flag.js
@@ -5,8 +5,40 @@ pwx.core.data.flag = pwx.core.data.flag || {}
 
 pwx.core.data.flag.current = function(){
 
-}
+    const states = {
+        black: _pwxProp( pwx.core.config.flag.black ),
+        blue: _pwxProp( pwx.core.config.flag.blue ),
+        caution: _pwxProp( pwx.core.config.flag.caution ),
+        cautionWaving: _pwxProp( pwx.core.config.flag.cautionWaving ),
+        checkered: _pwxProp( pwx.core.config.flag.checkered ),
+        crossed: _pwxProp( pwx.core.config.flag.crossed ),
+        debris: _pwxProp( pwx.core.config.flag.debris ),
+        disqualify: _pwxProp( pwx.core.config.flag.disqualify ),
+        lapToGreen: _pwxProp( pwx.core.config.flag.lapToGreen ),
+        red: _pwxProp( pwx.core.config.flag.red ),
+        repair: _pwxProp( pwx.core.config.flag.repair ),
+        white: _pwxProp( pwx.core.config.flag.white),
+        yellow: _pwxProp( pwx.core.config.flag.yellow),
+        yellowWaving: _pwxProp( pwx.core.config.flag.yellowWaving ),
+    }
 
-pwx.core.data.flag.is = function( state ){
-
+    if( states.red )
+        return 'red'
+    if( states.black || states.disqualify )
+        return 'black'
+    if( states.caution || states.cautionWaving || states.yellow || states.yellowWaving )
+        return 'yellow'
+    if( states.debris )
+        return 'debris'
+    if( states.blue )
+        return 'blue'
+    if( states.repair )
+        return 'meatball'
+    if( states.lapToGreen )
+        return 'formation'
+    if( states.white )
+        return 'white'
+    if( states.checkered )
+        return 'checkered'
+    return 'green'
 }
