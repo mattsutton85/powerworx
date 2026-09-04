1b63bd6a4e4487181761901feb2abce2e2663c00
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-04 17:34:44 +0100

    Update pwx.dash.js

--- Simhub/PWX-Dash/pwx.dash.js
+++ Simhub/PWX-Dash/pwx.dash.js
@@ -25,76 +25,77 @@ pwx.dash.state = function(){
 pwx.dash.shift.led.state = function( ledNum ){
 
     const state = pwx.dash.state()
+
     const shift = {
         progress: pwx.core.data.car.gearbox.shift.progress(),
         point: pwx.core.data.car.gearbox.shift.point(),
         blink: pwx.core.data.car.gearbox.shift.blink()
     }
 
+    const rpm = pwx.core.data.car.engine.rpm.current()
+
     let led = {
         num: ledNum,
         on: false,
         colour: pwx.core.theme.colour.transparent,
         blink: false,
-        outer: ( [ 1, 2, 15, 16 ].includes( ledNum ) )
+        outer: [ 1, 2, 15, 16 ].includes( ledNum )
     }
 
-    const rpm = pwx.core.data.car.engine.rpm.current()
-
+    // Normal progressive shift LEDs
     if( shift.progress >= ledNum ){
         led.on = true
         if( ledNum <= 7 ){
             led.colour = pwx.core.theme.colour.green
-        }else if( ledNum > 7 && ledNum <= 12 ){
+        }else if( ledNum <= 12 ){
             led.colour = pwx.core.theme.colour.yellow
-        }else if( ledNum > 12 ){
+        }else{
             led.colour = pwx.core.theme.colour.red
         }
     }
 
-    // Shift point - flash all LEDS green
+    // Shift point
     if( rpm >= shift.point ){
         led.on = true
         led.colour = pwx.core.theme.colour.green
         led.blink = true
     }
-    
-    // Redline - flash all LEDS red
+
+    // Redline takes priority over shift point
     if( rpm >= shift.blink ){
         led.on = true
         led.colour = pwx.core.theme.colour.red
         led.blink = true
     }
 
-    // Pit / Flag mode - 
-    if( [ 'pit', 'flag' ].includes( state.name ) ){
-        if( led.outer ){
+    // Context mode takes priority on outer LEDs
+    if( led.outer ){
+        if( state.name === 'pit' ){
+            led.on = true
+            led.colour = pwx.core.theme.colour.purple
+            led.blink = true
+        }else if( state.name === 'flag' ){
             led.on = true
             led.blink = true
-            if( state.name === 'pit'{
-                led.colour = pwx.core.theme.colour.purple
-            }else{
-                switch( state.flag ){
-                    case 'red':
-                        led.colour = pwx.core.theme.colour.red
-                        break
-                    case 'debris':
-                    case 'yellow':
-                        led.colour = pwx.core.theme.colour.yellow
-                        break
-                    case 'meatball':
-                        led.colour = pwx.core.theme.colour.orange
-                        break
-                    case 'blue':
-                        led.colour = pwx.core.theme.colour.blue
-                        break
-                    default:
-                        led.colour = pwx.core.theme.colour.white
-                        break
-                }
+            switch( state.flag ){
+                case 'red':
+                    led.colour = pwx.core.theme.colour.red
+                    break
+                case 'debris':
+                case 'yellow':
+                    led.colour = pwx.core.theme.colour.yellow
+                    break
+                case 'meatball':
+                    led.colour = pwx.core.theme.colour.orange
+                    break
+                case 'blue':
+                    led.colour = pwx.core.theme.colour.blue
+                    break
+                default:
+                    led.colour = pwx.core.theme.colour.white
+                    break
             }
         }
     }
-
     return led
 }
