2adee9325660458b85208bcb9cda42b9231de945
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-05 12:55:54 +0100

    Update pwx.core.data.driver.js

--- Simhub/PWX-Core/pwx.core.data.driver.js
+++ Simhub/PWX-Core/pwx.core.data.driver.js
@@ -3,16 +3,7 @@ pwx.core = pwx.core || {}
 pwx.core.data = pwx.core.data || {}
 pwx.core.data.driver = pwx.core.data.driver || {}
 
-// iRacing driver ID
-pwx.core.data.driver.id = function(){
-    return _pwxProp( pwx.core.config.driver.id )
-}
-// iRacing driver name
+// Driver name
 pwx.core.data.driver.name = function(){
     return _pwxProp( pwx.core.config.driver.name )
-}
-
-// iRacing driver iRating
-pwx.core.data.driver.iRating = function(){
-    return _pwxProp( pwx.core.config.driver.iRating )
 }
