61a102cbbe6a9d7d165e57bdac4b4783721d0342
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-06 16:50:57 +0100

    Update pwx.dash2.js

--- Simhub/PWX-Dash/pwx.dash2.js
+++ Simhub/PWX-Dash/pwx.dash2.js
@@ -1,12 +1,17 @@
 var pwx = pwx || {}
+pwx.core = pwx.core || {}
 pwx.dash = pwx.dash || {}
 
 pwx.dash.layers = function(){
-    
+    const pitBox = pwx.core.data.pit.box()
+    const pitLane = pwx.core.data.pit.lane()
+    const pitLimiter = pwx.core.data.pit.limiter()
+    const flag = pwx.core.data.flag.current()
     let layers = {
-        pit: false,
-        limiter: false,
-        flag: false
+        pitBox: pitBox,
+        pitLane: pitLane,
+        pitLimiter: pitLimiter,
+        flag: ( flag && flag.name !== 'green' )
     }
     return layers
 }
