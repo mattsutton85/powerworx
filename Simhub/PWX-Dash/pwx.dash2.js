f0fb81c8ba6b4a364d081075dabec3142690dea5
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-06 16:42:57 +0100

    Create pwx.dash2.js

---
+++ Simhub/PWX-Dash/pwx.dash2.js
@@ -0,0 +1,13 @@
+var pwx = pwx || {}
+pwx.dash = pwx.dash || {}
+
+pwx.dash.layers = function(){
+    
+    let layers = {
+        pit: false,
+        limiter: false,
+        flag: false
+    }
+    return layers
+}
+
