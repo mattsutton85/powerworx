ff140679305e03b3747376571cd8ec0e3349c973
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-06 17:10:01 +0100

    Update pwx.dash2.js

--- Simhub/PWX-Dash/pwx.dash2.js
+++ Simhub/PWX-Dash/pwx.dash2.js
@@ -16,31 +16,33 @@ pwx.dash.layers = function(){
     return layers
 }
 
-pwx.dash.flag = function(){
-    return pwx.core.data.flag.current()
-}
-
 pwx.dash.led = function( ledNum ){
-    
     const state = {
         rpm: pwx.core.data.engine.rpm(),
         shift: {
-            point: pwx.core.data
-        }
+            progress: pwx.core.data.car.gearbox.shift.progress(),
+            point: {
+                target: pwx.core.data.car.gearbox.shift.point(),
+                lead: 150,
+                overrun: 100
+            },
+            redline: pwx.core.data.car.gearbox.shift.blink()
+        },
+        ledOuter = ([1,2,15,16]).includes(ledNum)
     }
-    
     let led = {
         on: false,
         colour: pwx.core.config.theme.colour.transaprent.hex,
         flash: false,
         flashInterval: null
-    }
-    
-    const isOuter = ([1,2,15,16]).includes(ledNum)
-    const 
+    } 
 }
 
-pwx.dash.firmware.version = function(){
-    return _pwxString( '1.0' )
+pwx.dash.flag = function(){
+    return pwx.core.data.flag.current()
+}
+
+pwx.dash.version = function(){
+    return _pwxString( '1.0.1' )
 }
 
