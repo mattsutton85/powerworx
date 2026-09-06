41c60ef254b9a638e451edff9b50124d82f4c811
Matt Sutton <121235915+mattsutton85@users.noreply.github.com>
2026-09-06 17:57:24 +0100

    Update pwx.core.data.pit.js

--- Simhub/PWX-Core/pwx.core.data.pit.js
+++ Simhub/PWX-Core/pwx.core.data.pit.js
@@ -3,9 +3,12 @@ pwx.core = pwx.core || {}
 pwx.core.data = pwx.core.data || {}
 pwx.core.data.pit = pwx.core.data.pit || {}
 
+pwx.core.data.pit.box = function(){
+    return _pwxBoolean( _pwxProp( pwx.core.config.pit.box ) )
+}
+pwx.core.data.pit.lane = function(){
+    return _pwxBoolean( _pwxProp( pwx.core.config.pit.lane ) )
+}
 pwx.core.data.pit.limiter = function(){
     return _pwxBoolean( _pwxProp( pwx.core.config.pit.limiter ) )
-}
-pwx.core.data.pit.in = function(){
-    return _pwxBoolean( _pwxProp( pwx.core.config.pit.in ) )
 }
